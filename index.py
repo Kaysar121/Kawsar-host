0ef5857cec4c9c82edadee27f92dfa6fe87979c7
Kaysar121 <opkaysaryt2@gmail.com>
2026-02-07 20:24:40 +0600

    Delete index.py

--- index.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flask import jsonify
-
-@app.route('/')
-def index():
-    return jsonify({"message": "HOSTING WEBSITE IS ACTIVE", "status": "active"})
-
-if __name__ == '__main__':
-    app.run()
